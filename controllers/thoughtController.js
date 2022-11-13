f5ec3c327c9425675c52f9b250ac7e815714b12f
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 10:14:36 -0800

    added get thought by id to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -14,6 +14,24 @@ const thoughtController = {
   },
 
 
+  // get one thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then(dbThoughtData => {
+        // If no thought is found, send 404
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+}
+
 
 
 
