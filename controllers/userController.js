af8a9f40d661033d03278a0aa12fb789368cbef4
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 10:22:10 -0800

    added get all users to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,17 @@ const user = require(`../models/user`)
 
 const userController = {
     //methods 
-}
-
+      // get all users
+      getAllUser(req, res) {
+        User.find({})
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+    
+    
 
 
 
