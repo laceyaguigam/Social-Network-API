961b9f2214758a5afe9242a243eb808d56e9daca
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 09:22:15 -0800

    added the code for the middleware to server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
