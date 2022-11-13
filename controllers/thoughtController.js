741c996248132f3c3173aabe25c4f861d0d553b1
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 10:11:52 -0800

    added get all thoughts code to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,15 @@ const thought = require(`../models/thought`)
 
 const thoughtController = {
     //methods 
-}
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thoughts.find({})
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 
 
 
