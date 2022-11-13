1ef84e58365e0b893926703c10e6aed92cf1b948
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 11:09:44 -0800

    added create user to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,15 @@ const userController = {
             res.status(400).json(err);
           });
       },
-    }
+
+      // createUser
+createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+  
+}
 
 
 
