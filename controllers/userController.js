cfc612f2abe633422c103e06a6a25a686409f1a9
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 11:15:48 -0800

    added update user by id to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,13 +31,26 @@ const userController = {
           });
       },
 
-      // createUser
+      // create User
     createUser({ body }, res) {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
   },
   
+// update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUsesrData => {
+            if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+  },
+
 }
 
 
