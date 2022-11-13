6da6cf0431dfa229ca71a3f11982de4064670a88
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 09:27:04 -0800

    added code to connect mongoose to database in server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// required to connect them
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
@@ -6,8 +7,21 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// this is the middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+
+//tells mongoose which database to connect to
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+
+
 
