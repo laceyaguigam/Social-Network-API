286e0fa713598cf5afdbc95484d92d9b8475262a
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-13 11:30:28 -0800

    added get/post all and get/post delete one user to user route

---
+++ routes/api/user-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+// Set up GET all and POST at /api/user
+router
+  .route('/')
+  .get()
+  .post();
+
+// Set up GET one, PUT, and DELETE at /api/user/:id
+router
+  .route('/:id')
+  .get()
+  .put()
+  .delete();
+
+module.exports = router;
